9a0944368d43bb5c391c99e3c4e0d5ca15aa8e2a
winterhart <m.user.work@gmail.com>
2019-04-27 21:16:48 -0400

    Adding business rule to the views

--- views/contact.py
+++ views/contact.py
@@ -1,3 +1,4 @@
+import re
 from flask import Blueprint, make_response, jsonify, request
 from datasource.database import db
 from models.contact import Contact
@@ -6,6 +7,9 @@ from util.uuid_generator import UuidGenerator
 
 
 class ContactView:
+    """ View for handling contact """
+    # Regex from https://www.daniweb.com/programming/software-development/threads/406245/regular-expression-for-email
+
     contact = Blueprint('contact', __name__, url_prefix='/api')
 
     @contact.route('/contacts', methods=['GET'])
@@ -27,24 +31,39 @@ class ContactView:
         """ Creating a contact and saving in storage """
         try:
             content = request.get_json()
-
+            
             name = content['name']
             phone = None
             address = None
             email = None
+
             # If phone is not null take it
             if 'phone' in content:
-                phone = content['phone']
+                phone_only_number = re.sub("[^0-9]", "", content['phone'])
+                if phone_only_number.__len__() > 9 and phone_only_number.__len__() < 16:
+                    phone = content['phone']
+                else:
+                    raise Exception("Invalid Phone format")
 
             if 'address' in content:
                 address = content['address']
 
             if 'email' in content:
-                email = content['email']
+                # Validate format of email
+                email_reg = re.compile(r'(\w+[.|\w])*@(\w+[.])*\w+')
+                if email_reg.match(content['email']):
+                    email = content['email']
+                else:
+                    raise Exception("Invalid Email format")
 
             # If name is empty cancel transaction
             if not name:
                 raise Exception("Pre-requisite not respected")
+
+            
+            # Email or Phone or Address must be not None
+            if not email and not phone and not address:
+                raise Exception("No enough contact information")
 
             # Any other fields can be empty
             uid = UuidGenerator().generate_uuid()
